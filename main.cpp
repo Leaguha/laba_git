e209e99d1afb3baf96fdcec096209aa71116f479
leaguha <hbrbnf_2006@mail.ru>
2025-12-17 22:28:16 +0500

    главный каркас

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdint>
+#include <iomanip>
+
+
+using namespace std;
+
+struct Textbook
+{
+    string predmet;
+    int64_t stran;
+    string razmer;
+};
+
+
+int main()
+{
+    //ô-ÿ 1:
+
+    //ô-ÿ 2:
+
+    //ô-ÿ 3:
+
+    return 0;
+}
+
+
